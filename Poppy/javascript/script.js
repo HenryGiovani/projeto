3563eaecac22992bb683dabfdf45dbf3c138384e
HenryGiovani <101908063+HenryGiovani@users.noreply.github.com>
2023-05-08 21:07:23 -0300

    Add files via upload

---
+++ Poppy/javascript/script.js
@@ -0,0 +1,7 @@
+function abrirMenu() { /*função para abrir o menu*/
+    document.getElementById('menu').style.width = "250px";
+}
+
+function fecharMenu() { /*função para fechar o menu*/
+    document.getElementById('menu').style.width = "0px";
+}
